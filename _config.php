90e1f2d9a210f80430c648695d0bfb9dcf1896b6
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-04-09 04:39:42 +0000

    UPGRADE 3.1
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/sitemappage/trunk@5581 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -4,12 +4,3 @@
 **/
 
 
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START sitemappage MODULE ----------------===================
-//MUST SET
-//Object::add_extension('SiteTree', 'SitemapPageDecorator');
-
-// OPTIONAL - to control what pages are shown in sitemap... INCLUDES overrules EXCLUDE
-//SitemapPageDecorator::set_sitemap_classes_to_exclude(array("HomePage", "MenuPage")); OR
-//SitemapPageDecorator::set_sitemap_classes_to_include(array("ProductPage", "CarrotPage"));
-//===================---------------- END sitemappage MODULE ----------------===================
