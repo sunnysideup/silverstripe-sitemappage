660d10c1657cdaef5c9bcf4f7c986135d80232f2
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:22:51 +1300

    Converted to PSR-2

--- code/SiteMapPage.php
+++ code/SiteMapPage.php
@@ -8,51 +8,50 @@
  **/
 
 
-class SiteMapPage extends Page {
+class SiteMapPage extends Page
+{
 
-	private static $db = array(
-		"ShowAllPages" => "Boolean"
-	);
+    private static $db = array(
+        "ShowAllPages" => "Boolean"
+    );
 
-	private static $description = "Sitemap Page: shows all the pages of a site in a tree format";
+    private static $description = "Sitemap Page: shows all the pages of a site in a tree format";
 
-	private static $add_action = 'Site Map Page';
+    private static $add_action = 'Site Map Page';
 
-	private static $icon = 'sitemappage/images/treeicons/SiteMapPage';
+    private static $icon = 'sitemappage/images/treeicons/SiteMapPage';
 
-	public function canCreate($member = null) {
-		return ! SiteMapPage::get()->count();
-	}
+    public function canCreate($member = null)
+    {
+        return ! SiteMapPage::get()->count();
+    }
 
-	public function getCMSFields() {
-		$fields = parent::getCMSFields();
-		$fields->addFieldToTab("Root.Display", new CheckboxField("ShowAllPages", "Show all pages from the start"));
-		return $fields;
-	}
-
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->addFieldToTab("Root.Display", new CheckboxField("ShowAllPages", "Show all pages from the start"));
+        return $fields;
+    }
 }
 
-class SiteMapPage_Controller extends Page_Controller {
+class SiteMapPage_Controller extends Page_Controller
+{
 
-	public function init() {
-		parent::init();
-		if(!$this->ShowAllPages) {
-			Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-			Requirements::javascript('sitemappage/javascript/SiteMapPage.js');
-			Requirements::themedCSS('SiteMapPage', "sitemappage");
-		}
-		else {
-			Requirements::themedCSS('SiteMapPageOpen', "sitemappage");
-		}
-	}
+    public function init()
+    {
+        parent::init();
+        if (!$this->ShowAllPages) {
+            Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+            Requirements::javascript('sitemappage/javascript/SiteMapPage.js');
+            Requirements::themedCSS('SiteMapPage', "sitemappage");
+        } else {
+            Requirements::themedCSS('SiteMapPageOpen', "sitemappage");
+        }
+    }
 
-	public function LevelOneSiteMapPages() {
-		$homePage = SiteTree::get()->filter(array("ParentID" => 0))->first();
-		return $homePage->SiteMapPages($noParent = true);
-	}
-
-
+    public function LevelOneSiteMapPages()
+    {
+        $homePage = SiteTree::get()->filter(array("ParentID" => 0))->first();
+        return $homePage->SiteMapPages($noParent = true);
+    }
 }
-
-
-
